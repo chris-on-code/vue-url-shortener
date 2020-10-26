430abf1432c05b83a8f4c91942cc136b2cb0593e
Chris on Code <hey@chrisoncode.io>
2020-10-26 13:12:30 -0700

    stuff

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  im doing stuff
-  <p>more</p>
+  <div class="bg-purple-600 flex justify-center items-center min-h-screen">
+    <div class="bg-purple-900 text-purple-200 p-36 rounded-xl">
+      <p>header</p>
+      <p>shorten a url</p>
+      <p>footer</p>
+    </div>
+  </div>
 </template>
 
 <script>
