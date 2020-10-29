164ec554c444fcd9305657c97bb8a4915b2bc27c
Chris on Code <hey@chrisoncode.io>
2020-10-29 08:19:45 -0700

    moar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="bg-purple-600 flex justify-center items-center min-h-screen">
-    <div class="bg-purple-900 text-purple-200 p-36 rounded-xl">
-      <p>header</p>
-      <p>shorten a url</p>
-      <p>footer</p>
+  <div class="app min-h-screen justify-center items-center p-32">
+    <!-- header -->
+    <div class="rounded-lg p-6 text-center">
+      <h1 class="font-bold text-6xl text-white">
+        Shorten Your URL
+      </h1>
+    </div>
+
+    <!-- form -->
+    <div class="mt-12 max-w-3xl mx-auto">
+      <form action="">
+        <input
+          type="text"
+          class="w-full bg-transparent border-4 border-green-600 rounded-full p-6 mb-8 text-2xl"
+          placeholder="https://shorten-this-url.com"
+        />
+        <div class="text-right">
+          <button
+            class="py-6 px-10 bg-green-500 text-green-200 rounded-full text-2xl"
+          >
+            Shorten
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -13,3 +32,9 @@ export default {
   name: "App",
 };
 </script>
+
+<style>
+.app {
+  background: #000b48;
+}
+</style>
